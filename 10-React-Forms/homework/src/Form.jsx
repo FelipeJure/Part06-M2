fda03789cb829216a97934c955f87233f2a3dd16
FelipeJure <felipe.jure05@gmail.com>
2022-05-18 09:39:19 -0300

    hw forms

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import s from './Form.module.css';
+// import { useState } from 'react';
+import './index.css';
+
+
+
+export function validate (input) {
+  let errors = {}
+  if (!input.username) {
+    errors.username = 'Username is required';
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = 'Username is invalid';
+  }
+  if (!input.password) errors.password = 'Password is required'
+  else if (!/(?=.*[0-9])/.test(input.password)) errors.password = 'Password is invalid'
+
+  return errors;
+};
+
 
 export default function  Form() {
+  const [input, setInput] = React.useState({
+    username:'',
+    password:''
+  })
+const [errors,setErrors] = React.useState ({});
+
+  const handleInputChange = function(e) {
+    // e.preventDefault ();
+    setErrors(validate({
+      ...input,
+      [e.target.name]: e.target.value
+    }));     
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+
+  }
+
   return (
-      <div>
-        Componente Form
-      </div>
+      <form >
+        <div>
+          <label>Username:</label>
+          <input 
+            type='text'
+            className={errors.username ? `danger ${s.input}` : s.input} 
+            placeholder='example@gmail.com' 
+            name='username'
+            onChange={handleInputChange}
+            value={input.username}
+             />
+            {errors.username && (
+              <p className="danger">{errors.username}</p>
+            )}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input 
+            type='password' 
+            name='password'
+            value={input.password}
+            onChange={handleInputChange}
+            className={errors.password && 'danger'}/>
+            {errors.password && (
+              <p className="danger">{errors.password}</p>
+            )}
+        </div>
+        <input type='submit' className={s.submit}/>
+      </form>
   )
 }
